f2ba1b7b101fbdf32f6badda0162b58d52ebb383
Marcus Edel <marcus.edel@fu-berlin.de>
2020-06-06 23:00:42 +0200

    Add initial build script.

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir build
+cd build
+
+cmake \
+  -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+  -DCMAKE_PREFIX_PATH=${PREFIX} \
+  -DCMAKE_INSTALL_RPATH=${PREFIX}/lib \
+  -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=ON \
+  -DCMAKE_EXE_LINKER_FLAGS="-Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib" \
+  -DCMAKE_SHARED_LINKER_FLAGS="-Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib" \
+  -DCMAKE_BUILD_TYPE=Release \
+  $SRC_DIR
+
+make -j${CPU_COUNT}
+make install
