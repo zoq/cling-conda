622fbf03c8e0b07c443e7eb5227905ac7f0bb5ac
Marcus Edel <marcus.edel@fu-berlin.de>
2020-06-09 23:15:44 +0200

    Update commit as well.

--- .ci/hash.py
+++ .ci/hash.py
@@ -4,12 +4,16 @@ with open('meta.yaml', 'r') as f:
     content = f.read()
     content = content.split('\n')
     hash = os.popen('sha256sum master.tar.gz').read().split(' ')[0].strip()
+    commit = os.popen('cd cling && git rev-parse --short HEAD && cd ..').read().strip()
 
     update = ''
     for line in content:
-        update += line + '\n'
         if '{% set sha256' in line:
             update += '{% set sha256 = "' + hash + '" %}\n'
+        elif '{% set commit' in line:
+            update += '{% set commit = "' + commit + '" %}\n'
+        else:
+            update += line + '\n'
 
 update = update[0:-1]
 with open('meta.yaml', 'w') as f:
